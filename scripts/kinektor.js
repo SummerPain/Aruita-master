37b5df506dfdc610b2ac9cbffdabcd3a9ed9b949
SummerPain <dmi.kozhuch@gmail.com>
2020-11-04 01:57:56 +0300

    Balance and effects
    changed [Stairs to heaven] and [Kinektor]

--- scripts/kinektor.js
+++ scripts/kinektor.js
@@ -1,6 +1,6 @@
 const kinektor = extendContent(UnitType, "kinektor", {});
 kinektor.constructor = () => extend(UnitEntity, {});
 //force field: radius, regen, max, cooldown
-const ability = new JavaAdapter(ForceFieldAbility, {}, 30, 0.2, 400, 240);
+const ability = new JavaAdapter(ForceFieldAbility, {}, 28, 0.1, 200, 360);
 
 kinektor.abilities.add(ability);
